2929a7a0c682e2efd46e1c7847498c20b78b71f7
Yordan Yordanov <mixdata34@gmail.com>
2025-07-05 18:39:30 +0300

    Added an all books view and AllBooksViewModel. Also added BooksController and appropriate styles

---
+++ LibManage.ViewModels/AllBooksViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibManage.ViewModels
+{
+    public class AllBooksViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string Cover { get; set; }
+        public string BookType { get; set; }
+        public int Rating { get; set; }
+        public bool IsTaken { get; set; }
+    }
+}
---
+++ LibManage/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using LibManage.Data.Models.Library;
+using LibManage.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibManage.Web.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult All()
+        {
+            // Just for testing perposes untill the services are created
+            IEnumerable<AllBooksViewModel> books = new List<AllBooksViewModel>(){
+                new AllBooksViewModel()
+                {
+                    Title = "To kill a mockingbird",
+                    AuthorName = "Herper Lee",
+                    BookType = "physical",
+                    Rating = 5,
+                    IsTaken = false,
+                    Cover = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1612238791i/56916837.jpg",
+                    Id = Guid.NewGuid()
+                },
+                new AllBooksViewModel()
+                {
+                    Title = "Programming C# 10",
+                    AuthorName = "Ian Griffiths",
+                    BookType = "physical",
+                    Rating = 4,
+                    IsTaken = true,
+                    Cover = "https://libris.to/media/jacket/38834523o.jpg",
+                    Id = Guid.NewGuid()
+                },
+            }; 
+
+            return View(books);
+        }
+    }
+}
