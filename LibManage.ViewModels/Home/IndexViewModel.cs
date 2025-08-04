0a8b6c6ae66f7b25500cce31f867150902871013
Yordan Yordanov <mixdata34@gmail.com>
2025-08-04 15:54:10 +0300

    Improved the hero section of the website

---
+++ LibManage.ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LibManage.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public int BooksTotal { get; set; }
+        public int AuthorsTotal { get; set; }
+        public int PublishersTotal { get; set; }
+        public int ReviewsTotal { get; set; }
+    }
+}
