aa1b5e62ca1cd687607eef71d6518565b7c29e58
Yordan Yordanov <mixdata34@gmail.com>
2025-08-05 20:01:59 +0300

    Added custom error code pages

---
+++ LibManage/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibManage.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/404")]
+        public IActionResult Error404()
+        {
+            return View("Error404");
+        }
+        [Route("Error/500")]
+        public IActionResult ServerError()
+        {
+            return View("Error500");
+        }
+    }
+}
--- LibManage/Program.cs
+++ LibManage/Program.cs
@@ -66,9 +66,9 @@ public class Program
         }
         else
         {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            app.UseExceptionHandler("/Error/ServerError");
+            app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
         }
 
         app.UseHttpsRedirection();
